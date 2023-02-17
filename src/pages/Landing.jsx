1f1a83905776616b499cde0fd36fa8f4480f1410
Javier Delgado <99965499+Vanillajaviscript@users.noreply.github.com>
2023-02-16 19:03:08 -0500

    Adds button to landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,9 +1,20 @@
-
+// import logo from '../assets/images/logo.svg'
+// import main from '../assets/images/main.svg'
 const Landing = () => {
   return (
-    <div>
-
-    </div>
+    <main>
+      <nav>
+        {/* <img src={logo} alt="Job Tracker" className="logo" /> */}
+        <h4>Job Tracker</h4>
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>Job <span>tracking</span> app</h1>
+          <p>A place where I can keep track of all the jobs I have applied to.</p>
+          <button className="btn btn-hero">Login or Register</button>
+        </div>
+      </div>
+    </main>
   )
 };
 
