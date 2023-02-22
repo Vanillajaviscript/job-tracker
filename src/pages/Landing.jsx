c0d8d6753432e7aeda5a20472a0e259da9cd7feb
Javier Delgado <99965499+Vanillajaviscript@users.noreply.github.com>
2023-02-22 00:05:33 -0500

    Modifies index file and adds to landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,7 +1,8 @@
 import logo from '../assets/images/logo.svg'
 import main from '../assets/images/main.svg';
 import Wrapper from '../assets/wrappers/LandingPage';
-import Logo from '../components/Logo';
+import {Logo} from '../components';
+
 
 const Landing = () => {
   return (
